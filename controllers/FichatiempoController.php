5308a98365d3cbeed3724329d337643147dbc8e1
pabloaaab <paul6126@hotmail.com>
2019-03-03 21:54:56 -0500

    varip

--- controllers/FichatiempoController.php
+++ controllers/FichatiempoController.php
@@ -354,7 +354,8 @@ class FichatiempoController extends Controller
         header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
         header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
         header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-        header ('Pragma: public'); // HTTP/1.0        
+        header ('Pragma: public'); // HTTP/1.0 
+        return $this->redirect(['view', 'id' => $id]);
         $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);        
         $objWriter->save('php://output');                
         exit;        
