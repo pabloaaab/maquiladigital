758e27eda728ffcddc759bc0be42612957f904ad
pabloaaab <paul6126@hotmail.com>
2019-03-04 08:05:02 -0500

    varios

--- controllers/FichatiempoController.php
+++ controllers/FichatiempoController.php
@@ -361,15 +361,7 @@ class FichatiempoController extends Controller
         header("Content-Transfer-Encoding: binary ");
         $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);        
         $objWriter->save('php://output');        
-        exit;
-        
-
-//header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-//header("Content-Type: application/force-download");
-//header("Content-Type: application/octet-stream");
-//header("Content-Type: application/download");;
-//header("Content-Disposition: attachment;filename=$filename");
-//header("Content-Transfer-Encoding: binary ");    
+        exit; 
         
     }
 }
