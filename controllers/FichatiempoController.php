4984d17394b99e0bc32b19d4d5b87b1ac10b6add
pabloaaab <paul6126@hotmail.com>
2019-03-03 22:04:25 -0500

    varios

--- controllers/FichatiempoController.php
+++ controllers/FichatiempoController.php
@@ -351,14 +351,23 @@ class FichatiempoController extends Controller
         // If you're serving to IE 9, then the following may be needed
         header('Cache-Control: max-age=1');
         // If you're serving to IE over SSL, then the following may be needed
-        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
-        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-        header ('Pragma: public'); // HTTP/1.0 
-        
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+        header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header('Pragma: public'); // HTTP/1.0 
+        header("Content-Transfer-Encoding: binary ");
         $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);        
         $objWriter->save('php://output');
         return $this->redirect(['view', 'id' => $id]);
-        exit;        
+        exit;
+        
+
+//header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+//header("Content-Type: application/force-download");
+//header("Content-Type: application/octet-stream");
+//header("Content-Type: application/download");;
+//header("Content-Disposition: attachment;filename=$filename");
+//header("Content-Transfer-Encoding: binary ");    
+        
     }
 }
