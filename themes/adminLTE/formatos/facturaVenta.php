e09a7073eed166b09b2512ae3bf9cab60004cf73
pabloaaab <paul6126@hotmail.com>
2019-03-18 13:37:22 -0500

    varios

--- themes/adminLTE/formatos/facturaVenta.php
+++ themes/adminLTE/formatos/facturaVenta.php
@@ -74,8 +74,8 @@ class PDF extends FPDF {
         $this->Cell(118, 5, utf8_decode($factura->cliente->direccioncliente), 0, 0, 'l', 1);
         $this->SetFont('Arial', 'B', 7.5);
         $this->Cell(25, 5, utf8_decode("TIPO SERVICIO:"), 0, 0, 'J', 1);
-        $this->SetFont('Arial', '', 8);
-        $this->Cell(25, 5, utf8_decode($factura->ordenproduccion->tipo->tipo), 0, 0, 'L', 1);
+        $this->SetFont('Arial', '', 7.5);
+        $this->Cell(25, 5, utf8_decode(substr($factura->ordenproduccion->tipo->tipo,0,13)), 0, 0, 'L', 1);        
         $this->SetXY(10, 74);
         $this->SetFont('Arial', 'B', 8);
         $this->Cell(23, 5, utf8_decode("TELÉFONO:"), 0, 0, 'l', 1);
