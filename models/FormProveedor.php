31c61178ad30218e8e1eb728e5d110bea7f15bc4
coordinador de sistemas <csistemas@JGEFECTIVOS.LOCAL>
2019-09-09 13:34:17 -0500

    varios

--- models/FormProveedor.php
+++ models/FormProveedor.php
@@ -52,7 +52,7 @@ class FormProveedor extends Model
             ['cedulanit', 'match', 'pattern' => '/^[0-9\s]+$/i', 'message' => 'Sólo se aceptan números'],
             ['cedulanit', 'cedulanit_existe'],            
             [['dv'], 'string', 'max' => 1],
-            ['razonsocial', 'match', 'pattern' => '/^[0-a-záéíóúñ\s]+$/i', 'message' => 'Sólo se aceptan letras'],
+            ['razonsocial', 'match', 'pattern' => '/^[.-0-9a-záéíóúñ\s]+$/i', 'message' => 'Sólo se aceptan letras'],
             ['nombreproveedor', 'match', 'pattern' => '/^[a-záéíóúñ\s]+$/i', 'message' => 'Sólo se aceptan letras'],
             ['apellidoproveedor', 'match', 'pattern' => '/^[a-záéíóúñ\s]+$/i', 'message' => 'Sólo se aceptan letras'],
             ['apellidoproveedor', 'match', 'pattern' => '/^[a-záéíóúñ\s]+$/i', 'message' => 'Sólo se aceptan letras'],
